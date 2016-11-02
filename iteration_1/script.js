dd75e92806ec35839398a823ea60bf51c0eadf55
Kyle Yu <kyleyu@Kyles-MBP-2.wireless.newschool.edu>
2016-11-02 10:27:26 -0400

    iteration_1

---
+++ iteration_1/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	//append new element
+	for (i = 0; i < 100; i++){
+	  $("body").append( "<div class='element' style='background-color: hsl(" + i * 3.6 + ", 100%, 50%);'></div>" );
+	  console.log("element " + i + " added");
+}
+
+	
+	//mouse interaction
+	$('.element').mouseover(function(){
+		$(this).css('background-color','black');
+	})
+		$('.element').mouseout(function(){
+		$(this).css('background-color','#000000');
+	})
+})
